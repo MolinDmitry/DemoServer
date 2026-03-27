8468b1a8e0d4b522286a2fb019f814a3b6dedec1
Molin Dmitry <molin.dmitry@gmail.com>
2026-03-27 11:26:45 +0300

    demoserver-v.0.0.0-snapshot

---
+++ src/main.cpp
@@ -0,0 +1,210 @@
+// Для корректной работы freeaddrinfo в MinGW
+// Подробнее: http://stackoverflow.com/a/20306451
+#define _WIN32_WINNT 0x501
+
+
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <cinttypes> 
+
+
+
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+
+// Необходимо, чтобы линковка происходила с DLL-библиотекой 
+// Для работы с сокетами
+// также для успешной компиляции необходимо добавить аргумент компилятора -lws2_32 самым последним (в файле tasks.json)
+#pragma comment(lib, "libws2_32.a")
+
+using std::cerr;
+
+
+//============================================================================
+std::vector<std::string> splitString(std::string stringLine){
+    std::vector<std::string> strVector;
+
+    return strVector;
+}
+
+//=============================================================================
+std::vector<std::string> splitString(char* buf, size_t length){
+    std::vector<std::string> strVector;
+    std::string str = "";
+    for(size_t i = 0; i < length; i++){
+        str+= buf[i];
+    }
+    std::stringstream ss(str);
+    for(std::string str; std::getline(ss, str);){
+        strVector.push_back(str);
+    }
+    return strVector;
+}
+
+//==============================================================================
+int main(int argc, char* argv[]){
+    std::cout << "Demo simple Web-server" << std::endl;
+
+    // служебная структура для хранения информации
+    // о реализации Windows Sockets
+    WSADATA wsaData;
+
+    // старт использования библиотеки сокетов процессом
+    // (подгружается Ws2_32.dll)
+    int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+    // Если произошла ошибка подгрузки библиотеки
+    if (result != 0) {
+        cerr << "WSAStartup failed: " << result << "\n";
+        return result;
+    }
+
+    struct addrinfo* addr = NULL; // структура, хранящая информацию
+    // об IP-адресе  слущающего сокета
+
+    // Шаблон для инициализации структуры адреса
+    struct addrinfo hints;
+    ZeroMemory(&hints, sizeof(hints));
+
+    // AF_INET определяет, что используется сеть для работы с сокетом
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM; // Задаем потоковый тип сокета
+    hints.ai_protocol = IPPROTO_TCP; // Используем протокол TCP
+    // Сокет биндится на адрес, чтобы принимать входящие соединения
+    hints.ai_flags = AI_PASSIVE;
+
+    // Инициализируем структуру, хранящую адрес сокета - addr.
+    // HTTP-сервер будет висеть на 8000-м порту локалхоста
+    result = getaddrinfo("127.0.0.1", "8000", &hints, &addr);
+
+    // Если инициализация структуры адреса завершилась с ошибкой,
+    // выведем сообщением об этом и завершим выполнение программы 
+    if (result != 0) {
+        cerr << "getaddrinfo failed: " << result << "\n";
+        WSACleanup(); // выгрузка библиотеки Ws2_32.dll
+        return 1;
+    }
+
+    // Создание сокета
+    int listen_socket = socket(addr->ai_family, addr->ai_socktype,
+        addr->ai_protocol);
+    // Если создание сокета завершилось с ошибкой, выводим сообщение,
+    // освобождаем память, выделенную под структуру addr,
+    // выгружаем dll-библиотеку и закрываем программу
+    if (listen_socket == INVALID_SOCKET) {
+        cerr << "Error at socket: " << WSAGetLastError() << "\n";
+        freeaddrinfo(addr);
+        WSACleanup();
+        return 1;
+    }
+
+    // Привязываем сокет к IP-адресу
+    result = bind(listen_socket, addr->ai_addr, (int)addr->ai_addrlen);
+
+    // Если привязать адрес к сокету не удалось, то выводим сообщение
+    // об ошибке, освобождаем память, выделенную под структуру addr.
+    // и закрываем открытый сокет.
+    // Выгружаем DLL-библиотеку из памяти и закрываем программу.
+    if (result == SOCKET_ERROR) {
+        cerr << "bind failed with error: " << WSAGetLastError() << "\n";
+        freeaddrinfo(addr);
+        closesocket(listen_socket);
+        WSACleanup();
+        return 1;
+    }
+
+    // Инициализируем слушающий сокет
+    if (listen(listen_socket, SOMAXCONN) == SOCKET_ERROR) {
+        cerr << "listen failed with error: " << WSAGetLastError() << "\n";
+        closesocket(listen_socket);
+        WSACleanup();
+        return 1;
+    }
+
+    uint8_t finishFlag = 0;
+    while(!finishFlag){
+        // Принимаем входящие соединения
+        int client_socket = accept(listen_socket, NULL, NULL);
+        if (client_socket == INVALID_SOCKET) {
+            cerr << "accept failed: " << WSAGetLastError() << "\n";
+            closesocket(listen_socket);
+            WSACleanup();
+            return 1;
+        }
+
+        const int max_client_buffer_size = 1024;
+        char buf[max_client_buffer_size];
+
+        result = recv(client_socket, buf, max_client_buffer_size, 0);
+
+        std::stringstream response; // сюда будет записываться ответ клиенту
+        std::stringstream response_body; // тело ответа
+
+        if (result == SOCKET_ERROR) {
+            // ошибка получения данных
+            cerr << "recv failed: " << result << "\n";
+            closesocket(client_socket);
+        } else if (result == 0) {
+            // соединение закрыто клиентом
+            cerr << "connection closed...\n";
+        } else if (result > 0) {
+            // Мы знаем фактический размер полученных данных, поэтому ставим метку конца строки
+            // В буфере запроса.
+            buf[result] = '\0';
+            std::cout << buf << std::endl;
+
+            std::vector<std::string> strVect = splitString(buf, result);
+            std::cout << strVect[0] << std::endl;
+            // for(auto line:strVect){
+            //     std::cout << line << std::endl;
+            // }
+
+            // Данные успешно получены
+            // формируем тело ответа (HTML)
+            // response_body << "<title>Test C++ HTTP Server</title>\n"
+            //     << "<h1>Test page</h1>\n"
+            //     << "<p>This is body of the test page...</p>\n"
+            //     << "<h2>Request headers</h2>\n"
+            //     << "<pre>" << buf << "</pre>\n"
+            //     << "<em><small>Test C++ Http Server</small></em>\n";
+
+            response_body << "{\"param1\":0,\"param2\":1}\n";
+
+            // Формируем весь ответ вместе с заголовками
+            response << "HTTP/1.1 200 OK\r\n"
+                << "Version: HTTP/1.1\r\n"
+                << "Access-Control-Allow-Origin: http://localhost:8000\r\n"
+                << "Access-Control-Allow-Methods: GET, POST\r\n"
+                << "Access-Control-Allow-Headers: Content-Type\r\n"
+                << "Content-Type: application/json; charset=utf-8\r\n"
+                << "Content-Length: " << response_body.str().length()
+                << "\r\n\r\n"
+                << response_body.str();
+
+            // Отправляем ответ клиенту с помощью функции send
+            result = send(client_socket, response.str().c_str(),
+                response.str().length(), 0);
+
+            if (result == SOCKET_ERROR) {
+                // произошла ошибка при отправле данных
+                cerr << "send failed: " << WSAGetLastError() << "\n";
+            }
+            // Закрываем соединение к клиентом
+            closesocket(client_socket);
+    }
+    
+    }
+
+    // Убираем за собой
+    closesocket(listen_socket);
+    freeaddrinfo(addr);
+    WSACleanup();
+
+
+
+    system("pause"); 
+    return 0;
+}
