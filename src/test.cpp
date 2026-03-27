b927b286537dba8676a48eb7578df52bf1478ee1
Molin Dmitry <molin.dmitry@gmail.com>
2026-03-27 22:18:35 +0300

    version-0.0.3-snapshot

--- src/test.cpp
+++ src/test.cpp
@@ -17,4 +17,29 @@ namespace tests{
         return 0;
     }
 
+    int test02(){
+        std::cout << "Test №2" << std::endl;
+
+        QueryStructTypeDef queryObject = parseQuery("GET /testpage HTTP/1.1");
+        std::cout << queryObject.path << std::endl;
+        for(auto parameter: queryObject.parameters){
+            std::cout << parameter.parameterName << ": " << parameter.value << std::endl;
+        }
+
+        return 0;
+    }
+
+
+    int test03(){
+        std::cout << "Test №3" << std::endl;
+
+        QueryStructTypeDef queryObject = parseQuery("GET /testpage/ HTTP/1.1");
+        std::cout << queryObject.path << std::endl;
+        for(auto parameter: queryObject.parameters){
+            std::cout << parameter.parameterName << ": " << parameter.value << std::endl;
+        }
+
+        return 0;
+    }
+
 }
--- src/test.hpp
+++ src/test.hpp
@@ -3,4 +3,6 @@
 
 namespace tests{
     int test01(void);
+    int test02(void);
+    int test03(void);
 }
