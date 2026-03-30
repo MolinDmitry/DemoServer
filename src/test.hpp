c256d322c5c5c6c187cc3667acfb2ff62c83cdef
Molin Dmitry <molin.dmitry@gmail.com>
2026-03-30 07:58:14 +0300

    version-0.0.5-snapshot

--- src/test.hpp
+++ src/test.hpp
@@ -1,8 +1,46 @@
 
-
+//#include "queryparser.hpp"
 
 namespace tests{
     int test01(void);
     int test02(void);
     int test03(void);
+
+
+    int test01(){
+        std::cout << "Test №1" << std::endl;
+
+        QueryStructTypeDef queryObject = parseQuery("GET /demoserver/exec?command=com&param=value HTTP/1.1");
+        std::cout << queryObject.path << std::endl;
+        for(auto parameter: queryObject.parameters){
+            std::cout << parameter.parameterName << ": " << parameter.value << std::endl;
+        }
+
+        return 0;
+    }
+
+    int test02(){
+        std::cout << "Test №2" << std::endl;
+
+        QueryStructTypeDef queryObject = parseQuery("GET /testpage HTTP/1.1");
+        std::cout << queryObject.path << std::endl;
+        for(auto parameter: queryObject.parameters){
+            std::cout << parameter.parameterName << ": " << parameter.value << std::endl;
+        }
+
+        return 0;
+    }
+
+
+    int test03(){
+        std::cout << "Test №3" << std::endl;
+
+        QueryStructTypeDef queryObject = parseQuery("GET /testpage/ HTTP/1.1");
+        std::cout << queryObject.path << std::endl;
+        for(auto parameter: queryObject.parameters){
+            std::cout << parameter.parameterName << ": " << parameter.value << std::endl;
+        }
+
+        return 0;
+    }
 }
